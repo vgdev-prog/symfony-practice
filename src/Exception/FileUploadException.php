56ba1e04530e9a38e193de637a7fde6b438478c3
Vladyslav Honchar <dr.tienam123@gmail.com>
2025-10-19 23:14:58 +0000

    refactor(exception): improve file upload exception handling

--- src/Exception/FileUploadException.php
+++ src/Exception/FileUploadException.php
@@ -2,7 +2,20 @@
 
 namespace App\Exception;
 
-class FileUploadException
+class FileUploadException extends AbstractDomainException
 {
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
 
+    public function getDomainError(): string
+    {
+        return ErrorCode::FILE_UPLOAD_FAILED->value;
+    }
+
+    public function getHttpStatusCode():int
+    {
+        return 500;
+    }
 }
